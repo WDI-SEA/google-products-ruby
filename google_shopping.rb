81d6ce64d705306900f6674996a950efb8385029
k-buffum <knbuffum@gmail.com>
2016-02-29 20:41:01 -0800

    Number of Canon products found
    q

--- google_shopping.rb
+++ google_shopping.rb
@@ -10,38 +10,32 @@ data = google_data["items"]
 
 #
 kind_count = 0
-backorder_count = 0
-
+backorder = []
 product_many_images = []
+canon_products = []
 
 for i in (0...data.length) do
   # 1
-  #if data[i]["kind"] === "shopping#product"
-  #  kind_count += 1
-  #end
-
+  kind_count += 1 if data[i]["kind"] === "shopping#product"
+  
   # 2
-  #if data[i]["product"]["inventories"][0]["availability"].downcase === "backorder"
-  #  backorder_count += 1
-  #end
+  backorder << data[i] if data[i]["product"]["inventories"][0]["availability"].downcase === "backorder"
 
   # 3
   link_count = 0
   data[i]["product"]["images"].each do |image|
-    if image.has_key?("link")
-      link_count += 1
-    end
+    link_count += 1 if image.has_key?("link")
   end
-  if link_count > 1
-      product_many_images << data[i]["product"]["title"]
-  end
+  product_many_images << data[i]["product"]["title"] if link_count > 1
+  
+  #4
+  canon_products << data[i] if data[i]["product"]["brand"].downcase === "canon"
+
 
 end
+#puts data[0]
 
- 
-
-puts "Products with more than 1 image link: #{product_many_images}"
-#puts "Total items with kind shopping#product: #{kind_count}"
-#puts "Total number of items on backorder: #{backorder_count}"
-
-
+puts "Total items with kind shopping#product: #{kind_count}"
+puts "Total number of items on backorder: #{backorder.length}"
+puts "Products with more than 1 image link: #{product_many_images.length}"
+puts "Number of canon products: #{canon_products.length}"
