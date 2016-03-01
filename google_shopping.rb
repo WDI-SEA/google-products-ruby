a95c485a18969fb4b4dc3101bc3eb4cf53cd3a5d
k-buffum <knbuffum@gmail.com>
2016-02-29 20:21:26 -0800

    Problem 3 complete

--- google_shopping.rb
+++ google_shopping.rb
@@ -8,22 +8,40 @@ google_data = JSON.parse(file)
 #puts google_data["items"][0]["product"]["title"]
 data = google_data["items"]
 
+#
 kind_count = 0
 backorder_count = 0
 
+product_many_images = []
+
 for i in (0...data.length) do
   # 1
-  if data[i]["kind"] === "shopping#product"
-    kind_count += 1
-  end
+  #if data[i]["kind"] === "shopping#product"
+  #  kind_count += 1
+  #end
 
-  #2
-  if data[i]["product"]["inventories"][0]["availability"].downcase === "backorder"
-    backorder_count += 1
-  end
+  # 2
+  #if data[i]["product"]["inventories"][0]["availability"].downcase === "backorder"
+  #  backorder_count += 1
+  #end
 
+  # 3
+  link_count = 0
+  data[i]["product"]["images"].each do |image|
+    if image.has_key?("link")
+      link_count += 1
+    end
+  end
+  if link_count > 1
+      product_many_images << data[i]["product"]["title"]
+  end
 
 end
 
-puts "Total items with kind shopping#product: #{kind_count}"
-puts "Total number of items on backorder: #{backorder_count}"
+ 
+
+puts "Products with more than 1 image link: #{product_many_images}"
+#puts "Total items with kind shopping#product: #{kind_count}"
+#puts "Total number of items on backorder: #{backorder_count}"
+
+
