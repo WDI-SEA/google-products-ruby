33a56fb11c91ac785f5d3a8adbf68182072fbd3b
derek_robertson <dwilliamrobertson@gmail.com>
2016-07-05 00:43:26 -0700

    shopping data

--- lib/google_shopping.rb
+++ lib/google_shopping.rb
@@ -10,9 +10,69 @@ google_data = JSON.parse(file)
 
 # Use 'inspect' to see the contents of an array or hash
 # Hashes are similar to objects in JavaScript
-puts google_data.inspect
+# puts google_data.inspect
 
 # Hashes require bracket syntax
 # puts google_data['items'][0]['product']['title']
 
 # Put your answers below here
+
+
+count = 0
+#1st
+# google_data["items"].each do |item|
+#   if item["kind"] == "shopping#product"
+#     count += 1
+#   end
+# end
+
+# puts count
+
+#2nd
+
+# google_data["items"].each do |item|
+#   if item["product"]["inventories"][0]["availability"] == "backorder"
+#     puts item
+#   end
+# end
+
+#3rd
+
+# google_data["items"].each do |item|
+#   if item["product"]["images"].length > 1
+#     puts item
+#     count += 1
+#   end
+# end
+
+# puts count
+
+#4th
+
+# google_data["items"].each do |item|
+#   if item["product"]["brand"].downcase == "canon"
+#     puts item
+#     count += 1
+#   end
+# end
+
+# puts count
+
+#5th
+
+# google_data["items"].each do |item|
+#   if item["product"]["brand"].downcase == "canon" && item["product"]["author"]["name"] == "eBay"  
+#     puts item
+#     count += 1
+#   end
+# end
+
+# puts count
+
+#6th
+
+# google_data["items"].each do |item|
+#   puts item["product"]["brand"]
+#   puts item["product"]["inventories"][0]["price"]
+#   puts item["product"]["images"][0]
+# end
